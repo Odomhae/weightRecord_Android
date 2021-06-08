2a88b5ae38cf1f217c3a8ea455cfd702ddb2f72d
unknown <jihoon708@naver.com>
2021-06-08 22:09:11 +0900

    calendar library

--- app/src/main/java/com/odom/weightrecord/fragment/CalendarFragment.kt
+++ app/src/main/java/com/odom/weightrecord/fragment/CalendarFragment.kt
@@ -1,31 +1,33 @@
 package com.odom.weightrecord.fragment
 
 import android.os.Bundle
-import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.viewpager.widget.ViewPager
 import com.odom.weightrecord.R
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_calendar.*
-import java.text.SimpleDateFormat
 import java.util.*
 
 
 class CalendarFragment : Fragment(){
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
 
         setHasOptionsMenu(true)
 
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
         val button :Button = view.findViewById(R.id.bt_add_record)
+        val calendarView : com.applandeo.materialcalendarview.CalendarView = view.findViewById(R.id.calendarView)
+
+        calendarView.setOnDayClickListener { eventDay ->
+            val clickedDayCalendar = eventDay.calendar
+            Toast.makeText(requireContext(), clickedDayCalendar.toString(), Toast.LENGTH_SHORT).show()
+        }
 
         button.setOnClickListener {
 
@@ -40,10 +42,9 @@ class CalendarFragment : Fragment(){
                 val day = calendar[Calendar.DAY_OF_MONTH]
                 val month = calendar[Calendar.MONTH]
                 val year = calendar[Calendar.YEAR]
-                // val week = SimpleDateFormat("EE").format(calendar.time)
 
                 val day_full =
-                        year.toString() + "년 " + (month + 1) + "월 " + day + "일 "// + week + "요일"
+                        year.toString() + "년 " + (month + 1) + "월 " + day + "일 "
 
                 // 현지 시간 형식에 맞게
                 // getBestDateTimePattern
